f9928d2c045e2b28a408e46274a4bbd54283464b
crow <pengchengw@me.com>
2026-03-29 14:13:57 -0500

    align lightwell exception to core

--- src/server/game/Entities/Totem/Totem.cpp
+++ src/server/game/Entities/Totem/Totem.cpp
@@ -35,14 +35,18 @@ void Totem::Update(uint32 time)
 {
     Unit* owner = GetOwner();
 
-        // Lightwell exception removed in upstream, check owner->IsAlive() up front
-        if (!owner || !owner->IsAlive() || m_duration <= time)
+    if (!owner || !IsAlive() || m_duration <= time)
     {
         UnSummon();                                         // remove self
         return;
     }
 
     // If owner is dead and this is not a lightwell, despawn
+    if (!owner->IsAlive() && !(m_Properties && m_Properties->Type == SUMMON_TYPE_LIGHTWELL))
+    {
+        UnSummon();
+        return;
+    }
 
     m_duration -= time;
     Creature::Update(time);
