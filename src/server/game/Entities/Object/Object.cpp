1391a6751fa2d44d957cb3bc8ae1f7615990c080
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-08-01 02:02:55 +0200

    Update Object.cpp

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -513,10 +513,7 @@ void Object::BuildValuesUpdate(uint8 updateType, ByteBuffer* data, Player* targe
 void Object::AddToObjectUpdateIfNeeded()
 {
     if (m_inWorld && !m_objectUpdated)
-    {
-        AddToObjectUpdate();
-        m_objectUpdated = true;
-    }
+        m_objectUpdated = AddToObjectUpdate();
 }
 
 void Object::ClearUpdateMask(bool remove)
@@ -3115,9 +3112,10 @@ void WorldObject::GetCreaturesWithEntryInRange(std::list<Creature*>& creatureLis
     Cell::VisitObjects(this, searcher, radius);
 }
 
-void WorldObject::AddToObjectUpdate()
+bool WorldObject::AddToObjectUpdate()
 {
     GetMap()->AddUpdateObject(this);
+    return true;
 }
 
 void WorldObject::RemoveFromObjectUpdate()
